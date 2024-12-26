b26ebe453942a1659cea88c9066a99c7fe3f0f6a
Deepak A <deepak1906a@gmail.com>
2024-12-26 20:27:00 +0530

    done

--- fusion talk/fusion/fusion/settings.py
+++ fusion talk/fusion/fusion/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fusion_app', # Add the fusion_app to the INSTALLED_APPS list
 ]
 
 MIDDLEWARE = [
